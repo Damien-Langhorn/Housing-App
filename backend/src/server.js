fc35c391bdc75ffb92f4e08a24c5273476ed630c
Damien Langhorn <dlanghorn11@gmail.com>
2025-06-25 21:15:17 -0400

    env changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
